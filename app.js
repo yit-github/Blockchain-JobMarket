15c069738d7f3dcf5c9b4601a11752804282cbc9
Archchitha <archchitha@outlook.com>
2018-02-25 19:22:24 +0530

    x

--- app.js
+++ app.js
@@ -477,7 +477,26 @@ employee_profile.setprofile(web3.eth.defaultAccount, $("#rate").val(), $("#descr
 
 
 
-
+function upload() {
+      const reader = new FileReader();
+      reader.onloadend = function() {
+        const ipfs = window.IpfsApi('localhost', 5001) // Connect to IPFS
+        const buf = buffer.Buffer(reader.result) // Convert data into buffer
+        ipfs.files.add(buf, (err, result) => { // Upload buffer to IPFS
+          if(err) {
+            console.error(err)
+            return
+          }
+          let url = `https://ipfs.io/ipfs/${result[0].hash}`
+          console.log(`Url --> ${url}`)
+          document.getElementById("url").innerHTML= url
+          document.getElementById("url").href= url
+          document.getElementById("output").src = url
+        })
+      }
+      const photo = document.getElementById("photo");
+      reader.readAsArrayBuffer(photo.files[0]); // Read Provided File
+    }
 
 var jobContract = web3.eth.contract([
 	{
