6e910c9429b27a9f285ff28a06ba995f259e5cd2
janarthan <janarthan@finnovation.lk>
2018-03-01 16:35:24 +0530

    Add contract without validations

--- contracts/SimplerMarket.sol
+++ contracts/SimplerMarket.sol
@@ -1,7 +1,7 @@
 pragma solidity  ^0.4.18;
 
 contract Market {
-    //Runs out of gas
+    //has duplications
 
     enum Skill {JAVA, JS, SCALA, KOTLIN, OTHER}
 
@@ -27,16 +27,7 @@ contract Market {
         for(uint i=0; i<_skills.length; i++) {
             employee.skills.push(_skills[i]);
         }
-
-        bool alreadyIn = false;
-        for(uint e=0; e<employeeCodes.length; e++) {
-            if(msg.sender == employeeCodes[e]) {
-                alreadyIn = true;
-                break;
-            }
-        }
-        if(!alreadyIn)
-            employeeCodes.push(msg.sender) - 1;
+        employeeCodes.push(msg.sender) - 1;
     }
     function getEmployee(address _address) view public returns(string, uint[]) {
         return(employeeMap[_address].name, employeeMap[_address].skills);
@@ -51,16 +42,7 @@ contract Market {
         for(uint i=0; i<_vacants.length; i++) {
             employer.vacants.push(_vacants[i]);
         }
-
-        bool alreadyIn = false;
-        for(uint e=0; e<employerCodes.length; e++) {
-            if(msg.sender == employerCodes[e]) {
-                alreadyIn = true;
-                break;
-            }
-        }
-        if(!alreadyIn)
-            employerCodes.push(msg.sender) - 1;
+        employerCodes.push(msg.sender) - 1;
     }
     function getEmployer(address _address) view public returns(string, uint[]) {
         return (employerMap[_address].name, employerMap[_address].vacants);
@@ -93,12 +75,9 @@ contract Market {
     }
     function findEmployeesForSender() view public returns(address[] addresses) {
         address[] codes;
-
         var vacants = employerMap[msg.sender].vacants;
         for(uint v=0; v<vacants.length; v++) {
-
             for(uint e=0; e<employeeCodes.length; e++) {
-
                 uint[] skills = employeeMap[employeeCodes[e]].skills;
                 for(uint s=0; s<skills.length; s++) {
                     if(skills[s] == vacants[v]) {
@@ -106,29 +85,9 @@ contract Market {
                         break;
                     }
                 }
-
-            }
-
-        }
-
-        address[] distincts;
-        distincts[0] = codes[0];
-
-        for(uint c=0; c<codes.length; c++) {
-            bool duplicate = false;
-
-            for(uint d=c+1; d<codes.length; d++) {
-                if(codes[d] == codes[c]) {
-                    duplicate = true;
-                    break;
-                }
-            }
-
-            if(!duplicate) {
-                distincts.push(codes[c]);
             }
         }
-        return distincts;
+        return codes;
     }
 
     function findEmployer(bytes32 _skillInString) view public returns(address[] addresses) {
@@ -147,12 +106,9 @@ contract Market {
     }
     function findEmployersForSender() view public returns(address[] addresses) {
         address[] codes;
-
         var skills = employeeMap[msg.sender].skills;
         for(uint s=0; s<skills.length; s++) {
-
             for(uint e=0; e<employerCodes.length; e++) {
-
                 uint[] vacants = employerMap[employerCodes[e]].vacants;
                 for(uint v=0; v<vacants.length; v++) {
                     if(vacants[v] == skills[s]) {
@@ -160,29 +116,9 @@ contract Market {
                         break;
                     }
                 }
-
-            }
-
-        }
-
-        address[] distincts;
-        distincts[0] = codes[0];
-
-        for(uint c=0; c<codes.length; c++) {
-            bool duplicate = false;
-
-            for(uint d=c+1; d<codes.length; d++) {
-                if(codes[d] == codes[c]) {
-                    duplicate = true;
-                    break;
-                }
-            }
-
-            if(!duplicate) {
-                distincts.push(codes[c]);
             }
         }
-        return distincts;
+        return codes;
     }
 
     function _skillInUint(bytes32 _skill) view public returns(uint) {
